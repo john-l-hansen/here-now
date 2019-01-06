ac9dc3514d1cc858ebf4f306488b6fd748626c95
John Hansen <john@Johns-MacBook-Pro.local>
2019-01-06 15:02:01 -0800

    :rocket: initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <section id="header">
+                <div className="inner">
+                    <span className="icon major fa-cloud"></span>
+                    <h1>We want to put the choice back in the <br /> hands of mental health clients.</h1>
+                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
+                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
+                    <ul className="actions">
+                        <li><a href="#one" className="button scrolly">Discover</a></li>
+                    </ul>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,114 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+import Layout from '../components/layout';
+
+import pic01 from '../assets/images/pic01.jpg'
+import pic02 from '../assets/images/pic02.jpg'
+import pic03 from '../assets/images/pic03.jpg'
+import pic04 from '../assets/images/pic04.jpg'
+
+class Homepage extends React.Component {
+    render() {
+        const siteTitle = "hereNOW6";
+
+        return (
+            <Layout>
+                <Helmet title={siteTitle} />
+
+                {/* <section id="one" className="main style1">
+                    <div className="grid-wrapper">
+                        <div className="col-6">
+                            <header className="major">
+                                <h2>Lorem ipsum dolor adipiscing<br />
+                                amet dolor consequat</h2>
+                            </header>
+                            <p>Adipiscing a commodo ante nunc accumsan et interdum mi ante adipiscing. A nunc lobortis non nisl amet vis sed volutpat aclacus nascetur ac non. Lorem curae et ante amet sapien sed tempus adipiscing id accumsan.</p>
+                        </div>
+                        <div className="col-6">
+                            <span className="image fit"><img src={pic01} alt="" /></span>
+                        </div>
+                    </div>
+                </section> */}
+                
+                <section id="three" className="main style1 special">
+                    <div className="grid-wrapper">
+                        <div className="col-12">
+                            <header className="major">
+                                <h2>It's so simple</h2>
+                            </header>
+                            <p>Simply sign up, login and find either counselors or patients immediately.</p>
+                        </div>
+
+                        <div className="col-4">
+                            <span className="image fit"><img src={pic02} alt="" /></span>
+                            <h3>Sign Up</h3>
+                            <p>A super quick and easy sign up and you're ready to go!</p>
+                            <ul className="actions">
+                                {/* <li><a href="#" className="button">More</a></li> */}
+                            </ul>
+                        </div>
+                        <div className="col-4">
+                            <span className="image fit"><img src={pic03} alt="" /></span>
+                            <h3>Find</h3>
+                            <p>Quickly find counselors or patients near you, choose who and when.</p>
+                            <ul className="actions">
+                                {/* <li><a href="#" className="button">More</a></li> */}
+                            </ul>
+                        </div>
+                        <div className="col-4">
+                            <span className="image fit"><img src={pic04} alt="" /></span>
+                            <h3>Session</h3>
+                            <p>Engage in meaningful sessions tailored to what you need.</p>
+                            <ul className="actions">
+                                {/* <li><a href="#" className="button">More</a></li> */}
+                            </ul>
+                        </div>
+
+                    </div>
+                </section>
+
+                {/* This is an ugly large amount of icons left and copy right section
+
+                <section id="two" className="main style2">
+                    <div className="grid-wrapper">
+                        <div className="col-6">
+                            <ul className="major-icons">
+                                <li><span className="icon style1 major fa-code"></span></li>
+                                <li><span className="icon style2 major fa-bolt"></span></li>
+                                <li><span className="icon style3 major fa-camera-retro"></span></li>
+                                <li><span className="icon style4 major fa-cog"></span></li>
+                                <li><span className="icon style5 major fa-desktop"></span></li>
+                                <li><span className="icon style6 major fa-calendar"></span></li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <header className="major">
+                                <h2>Lorem ipsum dolor adipiscing<br />
+                                amet dolor consequat</h2>
+                            </header>
+                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
+                            <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
+                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
+                        </div>
+                    </div>
+                </section> */}
+
+                <section id="four" className="main style2 special">
+                    <div className="container">
+                        <header className="major">
+                            <h2>Interested in the project?</h2>
+                        </header>
+                        <p>Sign up for updates on the product development and early release.</p>
+                        <ul className="actions uniform">
+                            <li><a href="#" className="button special">Sign Up</a></li>
+                            <li><a href="#" className="button">Learn More</a></li>
+                        </ul>
+                    </div>
+                </section>
+            </Layout>
+        );
+    }
+}
+
+export default Homepage;
