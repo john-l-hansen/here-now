18520c24ce944b0afe9f39f90d7c9ba2bc72ac3d
John Hansen <john@Johns-MacBook-Pro.local>
2019-01-06 15:54:29 -0800

    update intro copy

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,36 @@ import React from 'react'
 import logoHereNow from '../assets/images/logo-here-now.svg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <section id="header">
-                <div className="inner">
-                <center><span className="image fit logo"><img src={logoHereNow} alt="hereNOW logo" /></span></center>
-                    {/* <span className="icon major fa-cloud"></span> */}
-                    <h1>We want to put the choice back in the <br /> hands of mental health clients.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
-                    <ul className="actions">
-                        <li><a href="#one" className="button scrolly">Learn More</a></li>
-                    </ul>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="header">
+        <div className="inner">
+          <center>
+            <span className="image fit logo">
+              <img src={logoHereNow} alt="hereNOW logo" />
+            </span>
+          </center>
+          {/* <span className="icon major fa-cloud"></span> */}
+          <h1>
+            We want to put the choice back in the <br /> hands of mental health
+            clients.
+          </h1>
+          <p>
+            We take the traditional setting of a client visit and bring it into
+            the 21 st century by providing <br /> on-demand access to volunteers,
+            counselors and mental health professionals.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button scrolly">
+                Learn More
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Header
